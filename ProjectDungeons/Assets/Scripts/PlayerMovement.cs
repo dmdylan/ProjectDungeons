84d2be4938b7ed23e43afcca365bb915032d6fe0
dmdylan <dylandanowski@gmail.com>
2020-01-15 07:27:00 -0500

    More input stuff

--- ProjectDungeons/Assets/Scripts/PlayerMovement.cs
+++ ProjectDungeons/Assets/Scripts/PlayerMovement.cs
@@ -1,30 +1,56 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    PlayerController playerControls;
+    //PlayerController playerControls;
+    Vector2 i_movement;
+    private void Awake()
+    {
+        //playerControls = new PlayerController();
+    }
 
-    private void Awake() => playerControls = new PlayerController();
-    private void OnEnable() => playerControls.Combat.Enable();
-    private void OnDisable() => playerControls.Combat.Disable();
+    private void OnEnable()
+    {
+       // playerControls.Combat.Enable();
+    }
+
+    private void OnDisable()
+    {
+       // playerControls.Combat.Disable();
+    }
+
     private void Update()
     {
-      
+        Move();
+    }
+
+    private void Move()
+    {
+        Vector3 movement = new Vector3(i_movement.x, 0, i_movement.y) * 5 * Time.deltaTime;
+        transform.Translate(movement);
+        Debug.Log(i_movement);
+    }
+
+    private void OnMove(InputValue value)
+    {
+        i_movement = value.Get<Vector2>();
+        
     }
 
     public void OnMovement(InputAction.CallbackContext context)
     {
-        var movementInput = playerControls.Combat.Movement.ReadValue<Vector2>();
-        Debug.Log(movementInput);
-        var movement = new Vector3
-        {
-            x = movementInput.x,
-            z = movementInput.y
-        };
-
-        transform.Translate(movement * 2 * Time.deltaTime);
+        //var movementInput = playerControls.Combat.Movement.ReadValue<Vector2>();
+        //Debug.Log(movementInput);
+        //var movement = new Vector3
+        //{
+        //    x = movementInput.x,
+        //    z = movementInput.y
+        //};
+        //
+        //transform.Translate(movement * 2 * Time.deltaTime);
     }   
 }
