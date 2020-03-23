f041031da6566d67969e9740449e0a165ca3ee69
dmdylan <dylandanowski@gmail.com>
2020-03-23 16:29:11 -0400

    Working on abilities

--- ProjectDungeons/Assets/Scripts/Scriptable_Objects/AbilityInfo.cs
+++ ProjectDungeons/Assets/Scripts/Scriptable_Objects/AbilityInfo.cs
@@ -3,18 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public enum DamageType { Physical, Magical }
+public enum EffectType { None, Stun, Slow, Snare}
 
 [CreateAssetMenu(fileName = "New Ability", menuName = "Ability")]
-public class AbilityInfo : ScriptableObject
+public abstract class AbilityInfo : ScriptableObject
 {
-
     [SerializeField] private string abilityName = "New Ability Name";
     [SerializeField] private string abilityToolTip = "New Ability Tooltip";
     [SerializeField] private float abilityCooldown = 0;
     [SerializeField] private float abilityBaseDamage = 0;
     [SerializeField] private float abilityScalingValue = 0;
+    [SerializeField] private float abilityRange = 0;
     [SerializeField] private int levelRequirement = 1;
-    [SerializeField] private DamageType abilityDamageType;
+    [SerializeField] private DamageType abilityDamageType = DamageType.Physical;
+    [SerializeField] private EffectType abilityEffectType = EffectType.None;
+    [SerializeField] private float effectDuration = 0;
     [SerializeField] private Sprite icon = null;
     private Color abilityColor = new Color();
 
@@ -23,8 +26,11 @@ public class AbilityInfo : ScriptableObject
     public float AbilityCooldown => abilityCooldown;
     public float AbilityBaseDamage => abilityBaseDamage;
     public float AbilityScalingValue => abilityScalingValue;
+    public float AbilityRange => abilityRange;
     public int LevelRequirement => levelRequirement;
     public DamageType AbilityDamageType => abilityDamageType;
+    public EffectType AbilityEffectType => abilityEffectType;
+    public float EffectDuration => effectDuration;
     public Sprite Icon => icon;
     public Color AbilityColor
     {
@@ -42,5 +48,9 @@ public class AbilityInfo : ScriptableObject
             return abilityColor;
         }
     }
+
+    public abstract void Initialize();
+    public abstract void OnTriggerAbility();
+
 }
 
